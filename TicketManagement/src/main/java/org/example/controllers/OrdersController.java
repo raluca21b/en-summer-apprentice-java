04fe3e16be69c11f2048196eec1ac8cf95cd2ffd
Raluca Bulz <ralubulz@gmail.com>
2023-07-20 15:45:43 +0300

    Optimised imports for OrdersController

--- TicketManagement/src/main/java/org/example/controllers/OrdersController.java
+++ TicketManagement/src/main/java/org/example/controllers/OrdersController.java
@@ -1,14 +1,11 @@
 package org.example.controllers;
 
-import org.example.dtos.EventDTO;
 import org.example.dtos.OrderDTO;
-import org.example.model.Event;
 import org.example.model.Order;
 import org.example.services.TicketsManagementService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
